1b537c3ca856f3644e0a40ebe61fe43b6682f593
Yago Lucas de Souza Freitas <yago.freitas@vortex.net.br>
2020-07-31 21:52:59 -0300

    configuração do server js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,9 @@
 import express from "express";
 
- const app = express();
+const app = express();
+
+const port = 3333;
+
+app.listen(port , () => {
+    console.log(`Servidor iniciado em localhost na porta ${port}`);
+});
