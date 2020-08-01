396d9f0e06a2ed81752734d11514cc2361f0bdeb
Yago Lucas de Souza Freitas <yago.freitas@vortex.net.br>
2020-07-31 22:00:09 -0300

    add user controller

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from "express";
+import UserController from "./app/controllers/userController";
 
 const app = express();
+app.use(express.json());
 
 const port = 3333;
+
+app.post('/users', UserController.store);
 
 app.listen(port , () => {
     console.log(`Servidor iniciado em localhost na porta ${port}`);
