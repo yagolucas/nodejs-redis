11d86e857b27f7fe3ba5a2cde11175bdcb406f1f
Yago Lucas de Souza Freitas <yago.freitas@vortex.net.br>
2020-07-31 22:22:07 -0300

    add configuração do nodemailer

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -1,3 +1,5 @@
+import Mail from '../lib/Mail';
+
 export default {
     async store(req, res) {
         const { name, email, password } = req.body;
@@ -6,6 +8,12 @@ export default {
             name, email, password
         }
 
+        await Mail.sendMail({
+            from: 'Yago Lucas teste <yagolucas.sf@gmail.com>',
+            to: `${name} <${email}>`,
+            sub: 'Primeiro email',
+            html: `Olá, ${name} bem vindo!!`
+        })
 
         return res.json(user);
     }
